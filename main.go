3c9d58b75165677d6ef54b31222632e4a5a04f90
Mehdi <mehdiab92@gmail.com>
2019-11-12 11:49:02 +0100

    adding new version api

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/mehdou92/vote-api/api"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,10 +18,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 // 	http.ListenAndServe(":8080", nil)
 // }
 
-func main() {
-	//print.Terminal("hello")
+// func main() {
+// 	//print.Terminal("hello")
 
-	r := gin.Default()
-	//handler.InitUser(r, postgresql.New())
-	r.Run(":8080")
+// 	r := gin.Default()
+// 	//handler.InitUser(r, postgresql.New())
+// 	r.Run(":8080")
+// }
+
+func main() {
+	api.Run()
 }
